f6ee6f72b1d42be54d71857708f177f4f9b9f844
Michael Pettit <subnormalnebula@gmail.com>
2019-10-09 16:58:24 +1100

    Added data get and convert functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from "axios"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      festivalBandArray: [],
+      loaded: false
+    }
+  }
+
+  componentDidMount() {
+    axios.get("https://cors-anywhere.herokuapp.com/http://eacodingtest.digital.energyaustralia.com.au/api/v1/festivals")
+    .then(response => {
+        console.log(response);
+        this.setState({
+          festivalBandArray: response.data,
+          loaded: true
+        })
+    }).catch(error => {
+        console.log(error)
+    }) 
+  }
+
+  render() {
+
+    const { festivalBandArray, loaded } = this.state
+
+    console.log(festivalBandArray);
+
+    if (!loaded) {
+      return("Loading...");
+    } else {
+
+      var labelList = [];
+      var exists = false;
+      var label;
+
+      for (var festival of festivalBandArray) {
+        for (var band of festival.bands) {
+            for(label in labelList) {
+              if (labelList[label].recordLabel == band.recordLabel) {
+                exists = true;
+                break;
+              }
+            }
+          if (!exists) {
+            labelList.push({recordLabel: band.recordLabel, bands: [{name: band.name, festival: festival.name}]})
+          } else {
+            labelList[label].bands.push({name: band.name, festival: festival.name});
+            exists = false;
+          }
+        }
+      }
+
+      console.log(labelList); // For debugging
+
+      const createRecordLabelList = ( label, i ) => {
+        if (label.recordLabel){
+          console.log(label.recordLabel); // For debugging 
+          return(
+            null
+          )
+        }
+      }
+
+      return (
+
+        <div className="App">
+          {labelList.map(createRecordLabelList)}
+        </div>
+
+      );
+    }
+  }
 }
 
 export default App;
